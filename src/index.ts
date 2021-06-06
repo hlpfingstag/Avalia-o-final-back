9dde103ff0c4f3e2d0cfb3f5367fda8583b5b895
hlpfingstag <hlpfingstag@gmail.com>
2021-06-06 17:53:57 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import express, { NextFunction, Request, Response } from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const app = express();
+app.use(express.json());
+
+class Scrap {
+  public id: string;
+  public description: string;
+  public detailing: string;
+
+  constructor(description: string, detailing: string) {
+    this.id = uuidv4();
+    this.description = description;
+    this.detailing = detailing;
+  }
+}
+
+const scraps: Scrap[] = [];
+
+//Create
+app.post("/scraps", (request: Request, response: Response) => {
+  const { description, detailing }: any = request.body;
+
+  const scrap = new Scrap(description, detailing);
+
+  scraps.push(scrap);
+
+  return response.json({ scrap });
+});
+
+//Read
+app.get("/scraps/:id", (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  const scrap = scraps.find((p) => p.id === id);
+
+  return response.json({ scrap });
+});
+
+app.get("/scraps", (request: Request, response: Response) => {
+  return response.json(scraps);
+});
+
+//Update
+app.put("/scraps/:id", (request: Request, response: Response) => {
+  const { description, detailing }: any = request.body;
+
+  const scrap = new Scrap(description, detailing);
+
+  scraps.push(scrap);
+
+  return response.json({ scrap });
+});
+
+//Delete
+app.delete("/scraps/:id", (request: Request, response: Response) => {
+  const { id } = request.params;
+
+  const scrapIndex = scraps.findIndex((p) => p.id === id);
+
+  scraps.slice(scrapIndex, 1);
+
+  return response.status(204).json();
+});
+
+//Porta 3000
+app.listen(3000, () => {
+  console.log("servidor rodando...");
+});
